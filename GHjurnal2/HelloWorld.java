afcfeff864fbe3c09964b96464823ed10fcf25b5
pnajahhidayani <Pnajahhidayanidex@gmail.com>
2021-09-30 13:17:38 +0700

    HelloWorld.java

---
+++ GHjurnal2/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld {
+	
+	public static void main (String []args){
+		String name = "Putri Najah Hidayani";
+		
+		String NIM = "6706213029";
+					
+		String Kelas = "D3 RPLA 45-05";
+				
+		Double ipk = 4.00;
+			
+		System.out.println ("Nama: " + name);	
+		
+		System.out.println ("NIM: " + NIM);
+	
+		System.out.println ("Kelas: " + Kelas);
+
+	System.out.println ("IPK: " + ipk);
+}
+}
